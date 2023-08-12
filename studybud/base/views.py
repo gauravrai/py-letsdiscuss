d256e581b74ae12498ff93c91d313a76cb4bc577
Gaurav <rai.grv@gmail.com>
2023-08-12 22:37:24 +0530

    Added form, models, views for topic

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hey there')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def rooms(request):
-    return HttpResponse('Rooms here')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
